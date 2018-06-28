1573392ba3c3f74386ce39fca37a2dbef213b037
王士江 <wangshijiang@icourt.cc>
2018-06-28 22:05:17 +0800

    修改变量名m 为w

--- pokemon/linear-regression.py
+++ pokemon/linear-regression.py
@@ -1,34 +1,34 @@
 import numpy as np
 
-# y = mx + b
-# m is slope, b is y-intercept
-def compute_error_for_line_given_points(b, m, points):
+# y = wx + b
+# w is slope, b is y-intercept
+def compute_error_for_line_given_points(b, w, points):
     totalError = 0
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
-        totalError += (y - (m * x + b)) ** 2
+        totalError += (y - (w * x + b)) ** 2
     return totalError / float(len(points))
 
-def step_gradient(b_current, m_current, points, learningRate):
+def step_gradient(b_current, w_current, points, learningRate):
     b_gradient = 0
-    m_gradient = 0
+    w_gradient = 0
     N = float(len(points))
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
-        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
-        m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+        b_gradient += -(2/N) * (y - ((w_current * x) + b_current))
+        w_gradient += -(2/N) * x * (y - ((w_current * x) + b_current))
     new_b = b_current - (learningRate * b_gradient)    
-    new_m = m_current - (learningRate * m_gradient)
-    return [new_b, new_m]
+    new_w = w_current - (learningRate * w_gradient)
+    return [new_b, new_w]
 
-def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+def gradient_descent_runner(points, starting_b, starting_w, learning_rate, num_iterations):
     b = starting_b
-    m = starting_m
+    w = starting_w
     for i in range(num_iterations):
-        b, m = step_gradient(b, m, np.array(points), learning_rate)
-    return [b, m]    
+        b, w = step_gradient(b, w, np.array(points), learning_rate)
+    return [b, w]    
 
 def run():
     points = []
@@ -43,10 +43,11 @@ def run():
             points.append([float(r[2]), float(r[14])])
     learning_rate = 0.0000001
     initial_b = 0
-    initial_m = 0
+    initial_w = 0
     num_iterations = 1000
-    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
+    [b, w] = gradient_descent_runner(points, initial_b, initial_w, learning_rate, num_iterations)
 
-    print("After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, np.array(points))))
+    print("After {0} iterations b = {1}, w = {2}, error = {3}".format(num_iterations, b, w, 
+        compute_error_for_line_given_points(b, w, np.array(points))))
 
 run()
