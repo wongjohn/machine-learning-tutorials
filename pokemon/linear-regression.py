98026e4f3c9608716450d0bdb7edfb0cec004f62
王士江 <wangshijiang@icourt.cc>
2018-06-28 21:39:47 +0800

    添加线性回归的实现

---
+++ pokemon/linear-regression.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+# y = mx + b
+# m is slope, b is y-intercept
+def compute_error_for_line_given_points(b, m, points):
+    totalError = 0
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        totalError += (y - (m * x + b)) ** 2
+    return totalError / float(len(points))
+
+def step_gradient(b_current, m_current, points, learningRate):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+        m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+    new_b = b_current - (learningRate * b_gradient)    
+    new_m = m_current - (learningRate * m_gradient)
+    return [new_b, new_m]
+
+def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+    b = starting_b
+    m = starting_m
+    for i in range(num_iterations):
+        b, m = step_gradient(b, m, np.array(points), learning_rate)
+    return [b, m]    
+
+def run():
+    points = []
+    for line in open('pokemon.csv'):
+        r = line.split(',')
+        # if r[2] == 'cp':
+        #     continue
+        # if r[1] == 'Pidgey':
+        # if r[1] == 'Weedle':
+        if r[1] == 'Caterpie':
+        # if r[1] == 'Eevee':
+            points.append([float(r[2]), float(r[14])])
+    learning_rate = 0.0000001
+    initial_b = 0
+    initial_m = 0
+    num_iterations = 1000
+    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
+
+    print("After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, np.array(points))))
+
+run()
